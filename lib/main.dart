407bd176e41d2bfc6e85a52cc2a76f9ab8717880
Hiroki Yoshii <yoshii@HirokinoMacBook-Pro.local>
2023-11-09 16:00:44 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'package:animation/walk.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GameWidget(
+          game: Walk(),
+        ),
+      ),
+    );
+  }
+}
