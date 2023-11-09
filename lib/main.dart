c892fb14deb9c73bcfa423c1ddc1be987c2ac797
Hiroki Yoshii <yoshii@HirokinoMacBook-Pro.local>
2023-11-09 18:39:53 +0900

    デバイスの向きを横に固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:animation/sample.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // デバイスの向きを横に固定する
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  runApp(const App());
+}
 
+/// アプリケーション
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -16,6 +26,7 @@ class App extends StatelessWidget {
   }
 }
 
+/// ホーム画面
 class Home extends StatelessWidget {
   const Home({super.key});
 
